2160c85b1fa309cd78cd72a1ad66ddf4913c52ec
naidunaveen <146354959+naidunaveen@users.noreply.github.com>
2023-11-02 00:07:33 +0530

    Update tomcatscript.sh

--- tomcatscript.sh
+++ tomcatscript.sh
@@ -12,6 +12,6 @@ sudo ufw allow from any to any port 9090 proto tcp
 
 #inside tomcat-users.xml paste below commands and allign properly
 
-sudo sed -i '56 i <role rolename="admin-gui"/>' /etc/tomcat9/tomcat-users.xml
-sudo sed -i '57 i <role rolename="manager-gui"/>' /etc/tomcat9/tomcat-users.xml
-sudo sed -i '58 i <user username="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
+#sudo sed -i '56 i <role rolename="admin-gui"/>' /etc/tomcat9/tomcat-users.xml
+#sudo sed -i '57 i <role rolename="manager-gui"/>' /etc/tomcat9/tomcat-users.xml
+#sudo sed -i '58 i <user username="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
